db4d36e5ebbaa936fb52403a0cd5bd2ec205716e
YinJi-betsy <jiyin12345678@gmail.com>
2025-10-15 17:41:22 +0800

    updata

--- rtl/buffer.v
+++ rtl/buffer.v
@@ -12,5 +12,38 @@ module bufffer(
     input           rd_en         ,   //读使能信号，接tx_req
     output[15:0]    rd_byte_num       //每帧读出的数据字节数，初步定为1024字节
 );
+//对于输出信号，先处理reg变量再输出
+reg                 r_frame_tx_start;
+
+//对于输入信号，处理打拍信号
+wire                w_frame_tx_done;
+
+wire                w_fifo_full;
+wire                w_fifo_full_1d
+
+assign              frame_tx_start = r_frame_tx_start;
+/*****************读数据*******************/
+//信号打拍
+always@(posedge rd_clk or negedge rst_n)
+begin
+    if(!rst_n)begin
+        w_frame_tx_done <= 0;
+        w_fifo_full_1d <= 0;
+    end
+    else begin
+        w_frame_tx_done <= frame_tx_done;
+        w_fifo_full_1d <= w_fifo_full;
+    end
+end
+//r_frame_tx_start
+always@(posedge rd_clk or negedge rst_n)
+begin
+    if(!rst_n)
+        r_frame_tx_start <= 0;
+    else if()               //当缓冲区满时拉高或者当前帧数据frame_tx_done之后拉高
+        r_frame_tx_start <= 1;
+    else
+        r_frame_tx_start <= 0;
+end
 
 endmodule
